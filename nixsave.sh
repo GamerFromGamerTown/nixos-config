50f29e9e73b3c399953e992f46c7e4582f780316
GamerFromGamerTown <100511215+GamerFromGamerTown@users.noreply.github.com>
2024-02-25 04:17:03 -0900

    Delete nixsave.sh

--- nixsave.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Ensure that the environment variables and paths are correct for sudo and git
-export PATH=/run/current-system/sw/bin:/run/current-system/sw/sbin:$PATH
-sudo git -C /etc/nixos add . && sudo git -C /etc/nixos commit -m "Automatic update"
